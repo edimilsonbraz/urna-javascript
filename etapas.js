d5e9262be59835d235b70950e96f1f534cfbc5cd
edimilsonbraz <edimilson.gt8@gmail.com>
2021-11-01 16:23:25 -0300

    Controlando toda DOM com document...

---
+++ etapas.js
@@ -0,0 +1,50 @@
+let etapas = [
+  {
+    titulo: 'VEREADOR',
+    numero: 5,
+    candidatos: [
+      {
+        numero: 92000,
+        nome: 'Fulano de Tal',
+        partido: 'ABC',
+        fotos: [
+          {url:'92100.png', legenda: 'Vereador'}
+        ]
+      },
+      {
+        numero: 93000,
+        nome: 'Beltrano da Silva',
+        partido: 'DEF',
+        fotos: [
+          {url:'93100.png', legenda: 'Vereador'}
+        ]
+      },  
+    ]
+  },
+  {
+    titulo: 'PREFEITO',
+    numero: 2,
+    candidatos: [
+      {
+        numero: 92,
+        nome: 'Ciclano de Tal',
+        partido: 'ABC',
+        vice: 'Milena',
+        fotos: [
+          {url:'92.png', legenda: 'Prefeito'},
+          {url:'92_1.png', legenda: 'Vice-Prefeito', small: true}
+        ]
+      },
+      {
+        numero: 93,
+        nome: 'Zulano de Tal',
+        partido: 'DEF',
+        Vice: 'Zelia',
+        fotos: [
+          {url:'93.png', legenda: 'Prefeito'},
+          {url:'93_1.png', legenda: 'Vice-Prefeito', small: true}
+        ]
+      }
+    ]
+  }
+];
